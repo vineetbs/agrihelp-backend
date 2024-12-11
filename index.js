bd743b74edc09e6801bb47aa86ae3ca010ac0e38
vineetbs <thevineetbimal@gmail.com>
2024-12-11 23:20:26 +0530

    refactor API endpoint and comment out server start

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const app = express();
 const PORT = 5000;
 
-app.get("/api/news", async (req, res) => {
+app.get("/", async (req, res) => {
   const apiKey = "1adce1c892844c078ebde9b4c4c89dcf";
   try {
     const response = await axios.get(
@@ -15,6 +15,6 @@ app.get("/api/news", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
