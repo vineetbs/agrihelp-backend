28677b4b9f8e265ed683d74f0269cb23e4cafd71
vineetbs <thevineetbimal@gmail.com>
2024-12-11 23:41:28 +0530

    update server port configuration to 8080

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 
