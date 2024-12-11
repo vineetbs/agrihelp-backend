5534a82bbe1feefb0e2e70e32620f6ae0ab865fb
vineetbs <thevineetbimal@gmail.com>
2024-12-12 00:05:48 +0530

    update API endpoint to fetch news articles using a valid API key

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 
 app.get("/", async (req, res) => {
-  // const apiKey = "1adce1c892844c078ebde9b4c4c89dcf";
+  const apiKey = "1adce1c892844c078ebde9b4c4c89dcf";
   try {
-    const response = await axios.get(`agrihelpbackend.up.railway.app`);
+    const response = await axios.get(
+      `https://newsapi.org/v2/everything?q=agriculture&language=en&apiKey=${apiKey}`
+    );
     res.json(response.data);
   } catch (error) {
     res.status(500).send("Error fetching news");
